16f3271e41a188b56bd896fc335f9aa6f31b0585
rayenomr123 <rayenomri23p@gmail.com>
2025-07-13 23:53:53 +0100

    Electron + React structure

---
+++ electron/main.js
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
+  });
+
+  if (app.isPackaged) {
+    const indexPath = path.join(process.resourcesPath, 'app.asar', 'dist', 'index.html');
+    win.loadFile(indexPath);
+  } else {
+    win.loadURL('http://localhost:5173');
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
